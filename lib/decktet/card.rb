94d7255c4116f0eee7b8418524777f0db2ab6c89
Brody Fischer <brodyf42@gmail.com>
2024-09-29 23:37:58 -0700

    WIP4

--- lib/decktet/card.rb
+++ lib/decktet/card.rb
@@ -3,6 +3,16 @@ module Decktet
     attr_reader :name, :rank, :suits, :types
     private_class_method :new
 
+    class << self
+      def available_groups
+        CARD_GROUPS.keys
+      end
+
+      def cards_from_groups(*groups)
+        groups.map{|group| CARD_GROUPS[group]}.flatten.map(&:dup)
+      end
+    end
+
     SUITS = [:moons, :suns, :waves, :leaves, :wyrms, :knots]
     RANKS = [:ace] + Array(2..9) + [:pawn, :court, :crown]
     TYPES = [:location, :personality, :event]
@@ -104,5 +114,6 @@ module Decktet
     private_constant :PAWNS
     private_constant :COURTS
     private_constant :CROWNS
+    private_constant :CARD_GROUPS
   end
 end
--- lib/decktet/deck.rb
+++ lib/decktet/deck.rb
@@ -1,5 +1,7 @@
 module Decktet
   class Deck
+    include Decktet::CardCollection
+
     attr_reader :cards
     private_class_method :new
 
@@ -12,45 +14,21 @@ module Decktet
         new(*TEMPLATES[template])
       end
 
-      available_templates.each do |template|
-        define method "new_#{template}" do
-          new_deck_from_template(template)
-        end
-      end
-
       def new_custom_deck(*groups)
         new(*groups)
       end
     end
 
-    def shuffle
-      @cards.shuffle!
-      self
-    end
-
-    def cut(depth: nil)
-      unless depth.nil?
-        raise ArgumentError.new('Invalid depth: value must be positive') if depth < 0
-        raise ArgumentError.new('Invalid depth: cannot be greater than deck size') if depth > @cards.count
-      end
-
-      depth ||= rand(1...@cards.count)
-      @cards.rotate!(depth)
-      self
-    end
-
-    def draw(count = 1)
-      @cards.shift(count)
-    end
-
     private
 
     def initialize(*groups)
-      unless groups.all?{|group| Card::CARD_GROUPS.keys.include? group}
-        raise ArgumentError.new("A deck can only be initialized with the following defined groups: #{Card::CARD_GROUPS.keys}")
+
+      puts "groups: #{groups}"
+      unless groups.all?{|group| Decktet::Card.available_groups.include? group}
+        raise ArgumentError.new("A deck can only be initialized with the following defined groups: #{Decktet::Card.available_groups}")
       end
 
-      @cards = groups.map{|group| Card::CARD_GROUPS[group]}.flatten.map(&:dup)
+      @cards = Decktet::Card.cards_from_groups(*groups)
     end
 
     TEMPLATES = {
